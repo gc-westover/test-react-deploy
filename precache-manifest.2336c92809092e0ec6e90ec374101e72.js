f528c0281ce1ae32bbc44855a0db62f63376abde
Greg W <gcwestover@gmail.com>
2020-07-23 12:05:57 -0400

    Updates

---
+++ precache-manifest.2336c92809092e0ec6e90ec374101e72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "919d183187c4a5fd7e56d182a183465f",
+    "url": "/test-react-deploy/index.html"
+  },
+  {
+    "revision": "ed8aeeb6e02e215849fb",
+    "url": "/test-react-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/test-react-deploy/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-react-deploy/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed8aeeb6e02e215849fb",
+    "url": "/test-react-deploy/static/js/main.da41047b.chunk.js"
+  },
+  {
+    "revision": "e655ba3b3bc4de4da734",
+    "url": "/test-react-deploy/static/js/runtime-main.a0e06928.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-react-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
